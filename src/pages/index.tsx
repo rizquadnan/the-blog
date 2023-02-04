e1cf890ce3fe209f7474724e4f34131901eacdc0
Muhammad Adnan R <madnanrizqullah@gmail.com>
2023-02-04 10:39:11 +0700

    chore: rename project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>My Blog</title>
-        <meta name="description" content="My Blog is a social media in form of blog posts. Share thoughts and ideas here! " />
+        <title>The Blog</title>
+        <meta name="description" content="The Blog is a social media in form of blog posts. Share thoughts and ideas here! " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        My Blog
+        The Blog
       </div>
     </>
   )
